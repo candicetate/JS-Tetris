aae4ad39c390c60df89cd4a3bff5d11eb2af4b16
Candice Tate <67024645+candicetate@users.noreply.github.com>
2020-08-21 17:57:09 -0600

    Set up html

--- scripts.js
+++ scripts.js
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Tetris in JS</title>
-  </head>
-  <body>
-    <h1>Tetris</h1>
-
-    <script src="scripts.js"></script>
-  </body>
-</html>
+document.addEventListener("DOMContentLoaded", () => {});
