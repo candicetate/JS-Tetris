034c28a1ef2f8eed4035a39445296772fe22d895
Candice Tate <67024645+candicetate@users.noreply.github.com>
2020-08-21 20:09:34 -0600

    Added rotation function

--- scripts.js
+++ scripts.js
@@ -153,4 +153,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     draw();
   }
+
+  //Rotate
+  function rotate() {
+    undraw();
+    currentRotation++;
+    if (currentRotation == current.length) {
+      currentRotation = 0;
+    }
+    current = theTetriminoes[random][currentRotation];
+  }
 });
